659fd533c6021830623b4c18f2f389dc74fb20c3
0xNaN <enzodicicco@gmail.com>
2015-11-19 10:40:32 +0100

    test if the alarm turnOn when pressure is low

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -1,15 +1,32 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class TestAlarm {
-
+	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+	final Sensor sensor = context.mock(Sensor.class);
+	
     @Test
     public void
     should_starts_off() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
+    }
+    
+    @Test
+    public void
+    should_turn_on_when_tire_is_low() {
+    	Alarm alarm = new Alarm(sensor, 1, 2);
+    	context.checking(new Expectations(){{
+    		oneOf(sensor).popNextPressurePsiValue(); will(returnValue(0.0));
+    	}});
+    	alarm.check();
+    	
+    	assertTrue(alarm.isAlarmOn());
     }
 }
