183620973a34c43daf768ee11e204363edccc6f2
0xNaN <enzodicicco@gmail.com>
2015-11-19 10:10:46 +0100

    renaming test

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -7,7 +7,8 @@ import static org.junit.Assert.*;
 public class TestAlarm {
 
     @Test
-    public void foo() {
+    public void
+    should_starts_off() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
     }
