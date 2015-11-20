1da0a36600c41911f3475f71f7f3dfd8d728f4a3
0xNaN <enzodicicco@gmail.com>
2015-11-20 12:44:41 +0100

    removing unused import

--- Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
+++ Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/HtmlTextConverter.java
@@ -3,7 +3,6 @@ package tddmicroexercises.textconvertor;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class HtmlTextConverter extends TextConverter
 {
