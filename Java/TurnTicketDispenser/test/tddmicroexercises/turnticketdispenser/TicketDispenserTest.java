9a705eca5f027a9ce3f20da3af84934f0c228e5d
0xNaN <enzodicicco@gmail.com>
2015-11-23 11:12:26 +0100

    refactoring test extracting TICKET_ONE, TICKET_ZERO and firstTicket, secondTicket

--- Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -9,6 +9,12 @@ public class TicketDispenserTest {
 	TurnNumberSequence turnNumberSequence;
 	TicketDispenser dispenser;
 	
+	TurnTicket TICKET_ZERO = new TurnTicket(0);
+	TurnTicket TICKET_ONE  = new TurnTicket(1);
+	
+	TurnTicket firstTicket;
+	TurnTicket secondTicket;
+	
 	@Before
 	public void setUp() {
 		turnNumberSequence = new TurnNumberSequence();
@@ -17,24 +23,24 @@ public class TicketDispenserTest {
 	
 	@Test public void
 	the_first_ticket_is_zero() {
-		assertEquals(new TurnTicket(0), dispenser.getTurnTicket());
+		assertEquals(TICKET_ZERO, dispenser.getTurnTicket());
 	}
 	
 	@Test public void
 	the_tickets_value_increments() {	
-		TurnTicket firstTicket = dispenser.getTurnTicket();
-		TurnTicket secondTicket = dispenser.getTurnTicket();
-		assertEquals(new TurnTicket(1), secondTicket);
+		firstTicket = dispenser.getTurnTicket();
+		secondTicket = dispenser.getTurnTicket();
+		assertEquals(TICKET_ONE, secondTicket);
 	}
 	
 	@Test public void
 	two_dispenser_doesnt_assign_the_same_ticket() {
 		TicketDispenser secondDispenser = new TicketDispenser(turnNumberSequence);
 		
-		TurnTicket firstTicket = dispenser.getTurnTicket();
-		TurnTicket secondTicket = secondDispenser.getTurnTicket();
+		firstTicket = dispenser.getTurnTicket();
+		secondTicket = secondDispenser.getTurnTicket();
 		
-		assertEquals(new TurnTicket(0), firstTicket);
-		assertEquals(new TurnTicket(1), secondTicket);
+		assertEquals(TICKET_ZERO, firstTicket);
+		assertEquals(TICKET_ONE, secondTicket);
 	}
 }
