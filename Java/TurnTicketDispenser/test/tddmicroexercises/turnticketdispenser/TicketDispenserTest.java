9701549347c3fc12e9434b9f1c6315751d76ff2f
0xNaN <enzodicicco@gmail.com>
2015-11-23 11:06:04 +0100

    refactoring test

--- Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -2,22 +2,26 @@ package tddmicroexercises.turnticketdispenser;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TicketDispenserTest {
+	TurnNumberSequence turnNumberSequence;
+	TicketDispenser dispenser;
+	
+	@Before
+	public void setUp() {
+		turnNumberSequence = new TurnNumberSequence();
+		dispenser = new TicketDispenser(turnNumberSequence);
+	}
 	
 	@Test public void
 	the_first_ticket_is_zero() {
-		TurnNumberSequence turnNumberSequence = new TurnNumberSequence();
-		TicketDispenser dispenser = new TicketDispenser(turnNumberSequence);
 		assertEquals(new TurnTicket(0), dispenser.getTurnTicket());
 	}
 	
 	@Test public void
-	the_tickets_value_increments() {
-		TurnNumberSequence turnNumberSequence = new TurnNumberSequence();
-		TicketDispenser dispenser = new TicketDispenser(turnNumberSequence);
-		
+	the_tickets_value_increments() {	
 		TurnTicket firstTicket = dispenser.getTurnTicket();
 		TurnTicket secondTicket = dispenser.getTurnTicket();
 		assertEquals(new TurnTicket(1), secondTicket);
