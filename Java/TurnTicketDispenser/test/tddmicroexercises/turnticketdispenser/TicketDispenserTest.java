951ba2752ed26f320528fb6614819dfd739320a7
0xNaN <enzodicicco@gmail.com>
2015-11-23 10:56:16 +0100

    renaming test

--- Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TicketDispenserTest extends TicketDispenser {
 
 	@Test public void
-	should_assign_a_ticket_of_zero() {
+	the_first_ticket_is_zero() {
 		TicketDispenser dispenser = new TicketDispenser();
 		assertEquals(new TurnTicket(0), dispenser.getTurnTicket());
 	}
