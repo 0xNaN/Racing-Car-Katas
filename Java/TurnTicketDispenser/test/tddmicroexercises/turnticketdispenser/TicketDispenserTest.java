67ab5bf56c04ecbc82e2199ff4e10f927fba2d62
0xNaN <enzodicicco@gmail.com>
2015-11-23 11:04:35 +0100

    test ticket increments

--- Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ Java/TurnTicketDispenser/test/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TicketDispenserTest {
+	
 	@Test public void
 	the_first_ticket_is_zero() {
 		TurnNumberSequence turnNumberSequence = new TurnNumberSequence();
@@ -12,4 +13,13 @@ public class TicketDispenserTest {
 		assertEquals(new TurnTicket(0), dispenser.getTurnTicket());
 	}
 	
+	@Test public void
+	the_tickets_value_increments() {
+		TurnNumberSequence turnNumberSequence = new TurnNumberSequence();
+		TicketDispenser dispenser = new TicketDispenser(turnNumberSequence);
+		
+		TurnTicket firstTicket = dispenser.getTurnTicket();
+		TurnTicket secondTicket = dispenser.getTurnTicket();
+		assertEquals(new TurnTicket(1), secondTicket);
+	}
 }
