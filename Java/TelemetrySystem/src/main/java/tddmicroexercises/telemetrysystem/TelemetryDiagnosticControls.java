b59ffdf1ded85f4d9ce5fbe1790fc339340758e6
0xNaN <enzodicicco@gmail.com>
2015-11-24 12:33:36 +0100

    fixing indentation

--- Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
+++ Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControls.java
@@ -4,34 +4,32 @@ public class TelemetryDiagnosticControls
 {
 	private final String DiagnosticChannelConnectionString = "*111#";
 	public static final String DIAGNOSTIC_MESSAGE = "AT#UD";
-	
-    private TelemetryClient client;
-    private String diagnosticInfo = "";
 
-        public TelemetryDiagnosticControls(TelemetryClient client)
-        {
-            this.client = client;
-        }
-        
-        public String getDiagnosticInfo(){
-            return diagnosticInfo;
-        }
-        
-        public void setDiagnosticInfo(String diagnosticInfo){
-            this.diagnosticInfo = diagnosticInfo;
-        }
- 
-        public void checkTransmission() throws Exception
-        {
-        	connect();
-            if(client.getOnlineStatus() == false)
-            	client.reconnect();
-            client.send(DIAGNOSTIC_MESSAGE);
-            diagnosticInfo = client.receive();
-        }
+	private TelemetryClient client;
+	private String diagnosticInfo = "";
 
-        private void connect() {
-            client.connect(DiagnosticChannelConnectionString);	
-        }
-        
+	public TelemetryDiagnosticControls(TelemetryClient client) {
+		this.client = client;
+	}
+
+	public String getDiagnosticInfo() {
+		return diagnosticInfo;
+	}
+
+	public void setDiagnosticInfo(String diagnosticInfo) {
+		this.diagnosticInfo = diagnosticInfo;
+	}
+
+	public void checkTransmission() throws Exception {
+		connect();
+		if(client.getOnlineStatus() == false)
+			client.reconnect();
+		client.send(DIAGNOSTIC_MESSAGE);
+		diagnosticInfo = client.receive();
+	}
+
+	private void connect() {
+		client.connect(DiagnosticChannelConnectionString);	
+	}
+
 }
--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -11,40 +11,40 @@ import org.junit.Test;
 public class TelemetryDiagnosticControlsTest
 {
 	final String DIAGNOSTIC_MESSAGE = "AT#UD";
-	
+
 	@Rule public JUnitRuleMockery context = new JUnitRuleMockery();
 	private final TelemetryClient client = context.mock(TelemetryClient.class);
 	TelemetryDiagnosticControls diagnosticControl;
-	
+
 	@Before
 	public void setUp() {
 		diagnosticControl = new TelemetryDiagnosticControls(client);		
 	}
-	
+
 	@Test (expected = Exception.class)
-    public void
-    should_throw_an_exception_if_failed_to_reconnect() throws Exception {
+	public void
+	should_throw_an_exception_if_failed_to_reconnect() throws Exception {
 		context.checking(new Expectations(){{
 			exactly(1).of(client).connect(with(any(String.class)));
 			exactly(1).of(client).getOnlineStatus(); will(returnValue(false));
 			exactly(1).of(client).reconnect(); will(throwException(new Exception()));
 		}});
 		diagnosticControl.checkTransmission();
-    }
-	
+	}
+
 	@Test
 	public void
-    should_set_the_message_received() throws Exception {
-		
+	should_set_the_message_received() throws Exception {
+
 		context.checking(new Expectations(){{
 			exactly(1).of(client).connect(with(any(String.class)));
 			exactly(1).of(client).getOnlineStatus(); will(returnValue(true));
 			exactly(1).of(client).send(with(DIAGNOSTIC_MESSAGE));
 			exactly(1).of(client).receive(); will(returnValue("msg"));
 		}});
-		
+
 		diagnosticControl.checkTransmission();
 		assertEquals("msg", diagnosticControl.getDiagnosticInfo());
-    }
-	
+	}
+
 }
